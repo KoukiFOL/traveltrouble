899e4de6e6914366e4eba11b1ca8d90e0e86249f
Furukawa Masato <miwamasato07@gmail.com>
2023-09-09 13:27:42 +0900

    header.dart作成

---
+++ lib/Compornents/header.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('とらぶった～'),
+        actions: <Widget>[
+          // ヘッダー内のアクションボタンをここに追加
+        ],
+      ),
+    );
+  }
+}
